3e9fe3a9211035cd557ab7a38abb5e5a0bcacb53
onehanduz <onehanduz@gmail.com>
2025-05-29 18:44:55 +0500

    ff

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,8 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7819436423:AAH75GlPPVDeCQyk90t7ci5pn24Ie6wclo8";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://localhost:3000";
+const web_link =
+  "https://6835b3b6772f1e0008e2af80--phenomenal-dolphin-b504e0.netlify.app/";
 
 bot.start((ctx) =>
   ctx.reply("Welcome :)))))", {
